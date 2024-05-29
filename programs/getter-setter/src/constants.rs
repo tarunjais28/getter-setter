b8dc56c0204bd4a43dd1ba3b01d1fc2e5025716d
tarunjais28 <tarunjais28@gmail.com>
2024-05-29 14:53:42 +0530

    Added module for constants

---
+++ programs/getter-setter/src/constants.rs
@@ -0,0 +1,10 @@
+use super::*;
+
+#[constant]
+pub const MAINTAINERS_TAG: &[u8] = b"maintainers";
+
+#[constant]
+pub const STORAGE_TAG: &[u8] = b"storage";
+
+#[constant]
+pub const WHITELIST_TAG: &[u8] = b"whitelist";
